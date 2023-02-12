814b77e00ef5d4b87c61575452c96e6eb422b132
sumitkhudaniya <sumitkhudaniya1996@gmail.com>
2023-02-11 22:44:15 -0500

    initial project setup

---
+++ src/app/profile/profile.service.ts
@@ -0,0 +1,212 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+  //baseUrl = environment.baseUrl;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  projects:any = [
+    {
+      id: 1,
+      title: 'Portfolio',
+      desc: 'A website to make online presence on the web throught valuable work',
+      imgUrl: 'assets/images/portfolioproject.jpg',
+      tech: 'Angular, HTML, Bootstrap'
+    },
+
+    {
+      id: 2,
+      title: 'Web Search Engine',
+      desc: 'A web application to search content and make accessing speed faster from database',
+      imgUrl: 'assets/images/Webproject.jpg',
+      tech: 'Jsoup, Angular, Java, Algorithm'
+    },
+
+
+    {
+      id: 3,
+      title: 'Student Management System',
+      desc: 'Spring Boot application to effectively manage student queris and return solutions',
+      imgUrl: 'assets/images/studentproject.jpg',
+      tech: 'Spring Boot, Java, Angular, Oracle, REST APIs'
+    },
+  ]
+  about2 = `Hello!, My name is Sumit and I enjoy creating things that live on the internet .
+  My interest in web development started back in 2014 when I decided to try editing custom Bootstrap themes -- turns out hacking together a custom reblog button taught me a lot about HTML & CSS!
+  Fast-forward to today, and I have had the privilege of working at a huge Financial Service Corporation in Toronto .Here are few technologies I've been working with recently: .• TypeScript .• Angular .• REST APIs .• Java .• SpringBoot . `
+
+   about = ""
+  resumeurl = "../../assets/resume/Sumit_Resume.pdf"
+
+  skillsData: any = [
+    {
+      'id': '1',
+      'skill': 'ANGULAR 2+',
+      'progress': '85%'
+    },
+    {
+      'id': '2',
+      'skill': 'NODE JS',
+      'progress': '80%'
+    },
+    {
+      'id': '3',
+      'skill': 'JAVASCRIPT JQUERY',
+      'progress': '80%'
+    },
+    {
+      'id': '4',
+      'skill': 'SQL , MYSQL,MONOGO, FIREBASE',
+      'progress': '75%'
+    },
+    {
+      'id': '5',
+      'skill': 'REACT JS',
+      'progress': '75%'
+    },
+    {
+      'id': '6',
+      'skill': 'PYTHON,JAVA',
+      'progress': '70%'
+    }
+  ];
+
+
+  educationData: any = [
+    {
+      'id': '1',
+      'from_to_year': '2015 - 2019',
+      'education': 'Bachelor\'s Degree',
+      'stream': 'Bachelor of Engineering',
+      'info': `Aditya Silver Oak Institute is GTU Affiliated and ranks 7th in Gujurat
+Completed B.E in Computer Engineering with 9.54 CGPA.
+Won the Best Student Award 3 times for excellent Academic records at College.
+Maintained above 9.1 spi in every semester of college academic .`,
+      'institution': 'ADITYA SILVER OAK INSTITUTE OF TECHNOLOGY, AHMEDABAD'
+    },
+    {
+      'id': '2',
+      'from_to_year': '2013 - 2015',
+      'education': 'Higher Secondary',
+      'stream': 'Science and Mathematics',
+      'institution': 'GYANDEEEP VIDHYALAYA,AHMEDABAD',
+      'info': `The High School was mainly focus on the bases of science, mathematics, and Chemistry
+There was also a good base on physics, mathematics and chemistry.
+Completed my high school with 78%.`
+    },
+    {
+      'id': '3',
+      'from_to_year': '2012 - 2013',
+      'education': 'Secondary  School',
+      'stream': 'Science and Mathematics',
+      'institution': 'GYANDEEEP VIDHYALAYA,AHMEDABAD',
+      'info': `The Secondary aims at Maths , English , Science, Social Science, Litrature and Languages.
+Completed my Secondary school with 84%.`
+    }
+  ];
+  exprienceData: any = [
+    {
+      id: 4,
+      company: 'Fidelity Investments',
+      location: 'Toronto',
+      timeline: 'Sept 2022 to Dec 2022',
+      role: 'Software Developer Intern',
+      work: 'Developed front end components using Angular, and REST APIs with SpringBoot for Fidelity Clients to access its products and services'
+
+    },
+    {
+      id: 3,
+      company: 'Inca Infotech Technologies',
+      location: 'IN',
+      timeline: 'June 2019 to June 2021',
+      role: 'Software Engineer',
+      work: `Developed payment transer REST APIs for YES and PNB bank to facilitate inner and outer bank money transfer .Debugged 40+ technical Commerce Tool issues with QA to unblock the company from technical debt in a 4-5 week span`
+    },
+  //   {
+  //     id: 2,
+  //     company: 'Growth99',
+  //     location: 'Remote',
+  //     timeline: '',
+  //     role: 'Frontend  Developer',
+  //     work: `Technologies : Angular , Node JS , MongoDB .
+  // Worked on the product of Growth99 which allowed other clients to come and integrate our product in their website .
+  // Worked on REST Api , Admin Dashboard , Appointment Booking for services and UI of the application .
+  // Created sections like templates for email, sms, landing pages, clinic, services, providers and triggers Automation.
+  // `
+  //   },
+
+  //   {
+  //     id: 1,
+  //     company: 'Tata Consultancy Services',
+  //     location: 'Gandhinagar and Pune',
+  //     timeline: 'June 2019 - Jan 2021',
+  //     role: 'Assistant System Engineer Trainee',
+  //     work: `Technologies : Angular , JavaSpring Boot , SQL. Apache Solr .
+
+  // Project 1 : Worked on uploading two excel sheets with bulk of data and finding the matches for each business .
+
+  // Project 2 : Worked on indexing the search results inside the application with bulk of data using Apache Solr and configuring the server with cron jobs and shell scripts .
+
+  // Worked on ranking the search results based on various criteria,features and delivering the accurate results based on generated scores.
+  // `
+  //   },
+
+
+  ]
+
+  extraCircularInfo: any = [
+    {
+      id:1,
+      title: 'Blogger',
+      description: '',
+      
+    },
+    {
+      id:1,
+      title: 'Github',
+      description: '',
+     
+    },
+    {
+      id:1,
+      title: 'Freelancing',
+      description: '',
+  
+    },
+  ]
+  // contactus(data: any): Observable<any> {
+  //   return this.http.post(this.baseUrl + 'contact', data);
+  // }
+
+  skills(): Observable<any> {
+    // return this.http.get(this.baseUrl + 'skills');
+    return this.skillsData;
+  }
+
+  getProjects(): Observable<any> {
+    // return this.http.get(this.baseUrl + 'skills');
+    return this.projects;
+  }
+  education(): Observable<any> {
+    // return this.http.get(this.baseUrl + 'education');
+    return this.educationData;
+  }
+
+  exprience(): Observable<any> {
+    // return this.http.get(this.baseUrl + 'exprience');
+    return this.exprienceData;
+  }
+
+  extraCircular(): Observable<any> {
+    // return this.http.get(this.baseUrl + 'exprience');
+    return this.extraCircularInfo;
+  }
+}
